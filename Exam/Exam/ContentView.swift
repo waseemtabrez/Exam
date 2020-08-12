ee6e4304f09074fc7178f0c782162d460fe94394
Waseem Tabrez <wastab001@gmail.com>
2020-08-12 12:35:37 -0500

    [InitialSetup]

---
+++ Exam/Exam/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Exam
+//
+//  Created by Waseem on 12/08/20.
+//  Copyright © 2020 Waseem. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
